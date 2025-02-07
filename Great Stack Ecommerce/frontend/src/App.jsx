52a1ba06a9dbbc0ce405a8a2270e9ff12346b3fe
Shashwatt699 <shashwatsaxena4545@gmail.com>
2025-02-08 00:10:30 +0530

    Cart

--- Great Stack Ecommerce/frontend/src/App.jsx
+++ Great Stack Ecommerce/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
@@ -12,25 +12,28 @@ import Orders from './pages/Orders'
 import Navbar from './components/navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'> 
-    <Navbar/>
-    <SearchBar/>
-    <Routes>
-      <Route path='/Home' element={<Home/>} />
-      <Route path='/collection' element={<Collection/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/contact' element={<Contact/>} />
-      <Route path='/product/:productId' element={<Product/>} />
-      <Route path='/cart' element={<Cart/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/orders' element={<Orders/>} />
-      <Route path='/place-order' element={< PlaceOrder/>} />
-      <Route path='*' element={<Home/>} />
-    </Routes>
-    <Footer/>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
+      <Navbar />
+      <SearchBar />
+      <Routes>
+        <Route path='/Home' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/place-order' element={< PlaceOrder />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
+      <Footer />
     </div>
   )
 }
--- Great Stack Ecommerce/frontend/src/pages/Product.jsx
+++ Great Stack Ecommerce/frontend/src/pages/Product.jsx
@@ -2,14 +2,16 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
 
   const { productId } = useParams();
-  const { products , currency } = useContext(ShopContext);
+  const { products , currency , addToCart} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
+  const [size,setSize] = useState('');
 
   const fetchProductData = async () => {
 
@@ -17,7 +19,6 @@ const Product = () => {
       if (item._id === productId) {
         setProductData(item)
         setImage(item.image[0])
-        console.log(item);
         return null;
       }
     })
@@ -30,7 +31,7 @@ const Product = () => {
   return productData ? (
     <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
       {/* --------------- Product Data ----------- */}
-      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+      <div className='flex gap-12  sm:gap-12 flex-col sm:flex-row'>
 
         {/* ------------------ Product Images ------------- */}
         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
@@ -63,13 +64,33 @@ const Product = () => {
                 <p>Select Size</p>
                 <div className='flex gap-2'>
                     {productData.sizes.map((item,index) =>(
-                      <button className={`cursor-pointer border py-2 px-4 bg-gray-100`} key={index}>{item}</button>
+                      <button onClick={()=>setSize(item)} className={`cursor-pointer border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
                     ))}
                 </div>
           </div>
+          <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5' />
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                    <p>100% Original Product</p>
+                    <p>Cash on Delivery is available on this product.</p>
+                    <p>Easy return and exchange policy within 7 days.</p>
+          </div>
         </div>
       </div>
+          {/* -----------------Desciption & Review Section----------------------- */ }
+          <div className="mt-20">
+                    <div className='flex'>
+                      <p className='border px-5 py-3 text-sm'>Description</p>
+                      <p className='border  px-5 py-3 text-sm'>Reviews (122)</p>
+                    </div>
+                    <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptates fuga deserunt quos ut natus reprehenderit eos nobis, consequatur eveniet deleniti nostrum velit cum, perferendis molestiae? Accusamus eos autem doloribus animi! Tempora incidunt quibusdam placeat corrupti quos minus saepe reiciendis doloremque suscipit eligendi, atque exercitationem eveniet dicta fugit. Dolor, soluta.</p>
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, laborum magnam exercitationem iusto qui, commodi architecto magni rerum labore ratione facilis. Animi optio mollitia non quisquam modi! Porro, neque aspernatur.</p>
+                    </div>
+          </div>
 
+          {/* -------------- display related products -------------- */}
+          <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : <div className="opacity-0"></div>
 }

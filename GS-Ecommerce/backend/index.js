884defc3830f27a9dfe625371cd8e6f68286126c
Shashwatt699 <shashwatsaxena4545@gmail.com>
2025-02-21 17:55:55 +0530

    new day

--- GS-Ecommerce/backend/index.js
+++ GS-Ecommerce/backend/index.js
@@ -17,7 +17,7 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL , 'http://localhost:9000'] ,
     methods: 'GET,POST,PUT,DELETE',
     credentials: true
 }))
